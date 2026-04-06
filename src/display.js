1991cbb45ac8867440ac9be57edf4cda628b6d76
Boki <bojan.sejnjanovic@gmail.com>
2026-04-06 19:50:59 +0200

    initial layout design

--- src/display.js
+++ src/display.js
@@ -1,19 +1,55 @@
-const main = document.getElementById("content")
+function makeChildOf(parent, tag, properties) {
+  const child = document.createElement(tag);
+
+  for (const [property, value] of Object.entries(properties)) {
+    child[property] = value;
+  }
+
+  parent.appendChild(child);
+  return child;
+}
+
+function makeTempRange(parent, max, min) {
+  const tempRange = makeChildOf(parent, "div", {"className": "temp-range"})
+  makeChildOf(tempRange, "div", {"textContent": max})
+  makeChildOf(tempRange, "div", {"textContent": min})
+
+  return tempRange
+}
+
+function makeBottomRow(parent, uv, wd, ws, rp) {
+  const bottom = makeChildOf(parent, "div", {"className": "bottom"})
+  makeChildOf(bottom, "div", {"textContent": `UV ${uv}`})
+  makeChildOf(bottom, "div", {"textContent": wd})
+  makeChildOf(bottom, "div", {"textContent": ws})
+  makeChildOf(bottom, "div", {"textContent": rp})
+
+  return bottom
+}
 
 export function updateDisplay(data) {
-  const todayInfo = document.createElement("div")
-  todayInfo.className = "info"
-  const cityName = document.createElement("div")
-  cityName.className = "city-name"
-  const todayTemp = document.createElement("div")
-  todayTemp.className = "temp main"
-
-  cityName.textContent = data.city
-  todayTemp.textContent = data.currentTemp
-
-  todayInfo.appendChild(cityName)
-  todayInfo.appendChild(todayTemp)
-
+  const main = document.getElementById("content")
   main.innerHTML = ""
-  main.appendChild(todayInfo)
+
+  const todayInfo = makeChildOf(main, "div", {"className": "info"})
+  makeChildOf(todayInfo, "div", {"textContent": data.city})
+
+  const middle = makeChildOf(todayInfo, "div", {"className": "middle"})
+  makeChildOf(middle, "div", {"textContent": data.currentIcon})
+  makeChildOf(middle, "div", {"className": "temp", "textContent": data.currentTemp})
+  makeTempRange(middle, data.todayMax, data.todayMin)
+
+  makeBottomRow(todayInfo, data.uvIndex, data.windDirection, data.windSpeed, data.rainProb)
+  makeChildOf(todayInfo, "hr", {})
+
+  const forecast = makeChildOf(todayInfo, "div", {"className": "bottom"})
+
+  for (let i = 1; i < 4; i++) {
+    const box = makeChildOf(forecast, "div", {"className": "box"})
+    makeChildOf(box, "div", {"textContent": "DAY"})
+    const middle = makeChildOf(box, "div", {"className": "middle"})
+    makeChildOf(middle, "div", {"textContent": data[`day${i}icon`]})
+    makeTempRange(middle, data[`day${i}Max`], data[`day${i}Min`])
+    makeBottomRow(box, data[`day${i}uv`], data[`day${i}wd`], data[`day${i}ws`], data[`day${i}rp`])
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./styles.css"
 import { updateDisplay } from "./display";
 
 async function getData(location, isChecked) {
-  // try {
     const unit = isChecked ? "us" : "metric"
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&key=WDYABNRBGXHMMTQQH89WYKZ9D`;
     const response = await fetch(url);
@@ -12,24 +11,45 @@ async function getData(location, isChecked) {
 
     const data = await response.json();
     return data;
-  // } catch (error) {
-  //   console.error("An error occurred:", error.message);
-  // }
 }
 
 function processData(data) {
   console.log(data);
   return {
     city: data.resolvedAddress,
-    currentConditions: data.currentConditions.conditions,
+    currentIcon: data.currentConditions.icon,
     currentTemp: data.currentConditions.temp,
     todayMax: data.days["0"].tempmax,
     todayMin: data.days["0"].tempmin,
+    rainProb: data.currentConditions.precipprob,
+    uvIndex: data.currentConditions.uvindex,
+    windDirection: data.currentConditions.winddir,
+    windSpeed: data.currentConditions.windspeed,
+    day1Max: data.days["1"].tempmax,
+    day1Min: data.days["1"].tempmin,
+    day1icon: data.days["1"].icon,
+    day1rp: data.days["1"].precipprob,
+    day1uv: data.days["1"].uvindex,
+    day1wd: data.days["1"].winddir,
+    day1ws: data.days["1"].windspeed,
+    day2Max: data.days["2"].tempmax,
+    day2Min: data.days["2"].tempmin,
+    day2icon: data.days["2"].icon,
+    day2rp: data.days["2"].precipprob,
+    day2uv: data.days["2"].uvindex,
+    day2wd: data.days["2"].winddir,
+    day2ws: data.days["2"].windspeed,
+    day3Max: data.days["3"].tempmax,
+    day3Min: data.days["3"].tempmin,
+    day3icon: data.days["3"].icon,
+    day3rp: data.days["3"].precipprob,
+    day3uv: data.days["3"].uvindex,
+    day3wd: data.days["3"].winddir,
+    day3ws: data.days["3"].windspeed,
   };
 }
 
 const form = document.querySelector("form");
-
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   const loc = form.querySelector("input[type='text']").value.trim();
